4d44487b01e26fdd2052effcdc52ac0b957fadaf
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-07-18 13:03:32 -0300

    implementing Lombok

--- backend/src/main/java/com/devsaadeh/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsaadeh/dscatalog/entities/Category.java
@@ -1,45 +1,16 @@
 package com.devsaadeh.dscatalog.entities;
 
+import lombok.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Category implements Serializable {
     private Long id;
     private String name;
-
-    public Category() {
-    }
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return Objects.equals(id, category.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
--- backend/src/main/java/com/devsaadeh/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsaadeh/dscatalog/resources/CategoryResource.java
@@ -18,7 +18,8 @@ public class CategoryResource {
     public ResponseEntity<List<Category>> findAll(){
         List<Category> list = new ArrayList<>();
         list.add(new Category(1L,"Books"));
-        list.add(new Category(1L,"Electronics"));
+        list.add(new Category(2L,"Electronics"));
+
         return ResponseEntity.ok().body(list);
     }
 }
