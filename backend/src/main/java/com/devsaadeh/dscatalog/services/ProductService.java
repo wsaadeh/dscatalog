a326dafbcbb8fcbcdfba25a0f61326cef27e01e9
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-07-20 19:02:21 -0300

    POST and PUT products

--- backend/src/main/java/com/devsaadeh/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsaadeh/dscatalog/services/ProductService.java
@@ -5,6 +5,7 @@ import com.devsaadeh.dscatalog.dto.ProductDTO;
 import com.devsaadeh.dscatalog.entities.Category;
 import com.devsaadeh.dscatalog.entities.Product;
 import com.devsaadeh.dscatalog.entities.Product;
+import com.devsaadeh.dscatalog.repositories.CategoryRepository;
 import com.devsaadeh.dscatalog.repositories.ProductRepository;
 import com.devsaadeh.dscatalog.repositories.ProductRepository;
 import com.devsaadeh.dscatalog.services.exception.DatabaseException;
@@ -25,6 +26,9 @@ public class ProductService {
 
     @Autowired
     ProductRepository repository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public List<ProductDTO> findAll() {
@@ -48,27 +52,28 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
+        copyDtoToEntity(entity,dto);
+        entity = repository.save(entity);
+        return new ProductDTO(entity,entity.getCategories());
+    }
+
+    private void copyDtoToEntity(Product entity, ProductDTO dto) {
         entity.setName(dto.getName());
         entity.setPrice(dto.getPrice());
         entity.setDescription(dto.getDescription());
         entity.setImgUrl(dto.getImgUrl());
         entity.setDate(dto.getDate());
-//        dto.getCategories().forEach(x -> entity.getCategories().add(new Category(x.getId(), x.getName())));
-        entity = repository.save(entity);
-        return new ProductDTO(entity);
+        entity.getCategories().clear();//Limpar lista de categorias
+        dto.getCategories().forEach(x -> entity.getCategories().add( categoryRepository.getReferenceById(x.getId())));
     }
 
     @Transactional
     public ProductDTO update(Long id,ProductDTO dto) {
         try {
             Product entity = repository.getReferenceById(id);
-            entity.setName(dto.getName());
-            entity.setPrice(dto.getPrice());
-            entity.setDescription(dto.getDescription());
-            entity.setImgUrl(dto.getImgUrl());
-            entity.setDate(dto.getDate());
+            copyDtoToEntity(entity,dto);
             repository.save(entity);
-            return new ProductDTO(entity);
+            return new ProductDTO(entity,entity.getCategories());
         }catch (EntityNotFoundException e){
             throw new ResourceNotFoundException("Id not found " + id);
         }
