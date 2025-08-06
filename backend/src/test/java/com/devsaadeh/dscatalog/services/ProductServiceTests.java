839d4d9f83c5a8e9d38cee9c7e7f6889c1f17687
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-08-06 20:03:12 -0300

    createing ProductServiceTests

--- backend/src/test/java/com/devsaadeh/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsaadeh/dscatalog/services/ProductServiceTests.java
@@ -1,15 +1,32 @@
 package com.devsaadeh.dscatalog.services;
 
+import com.devsaadeh.dscatalog.entities.Product;
 import com.devsaadeh.dscatalog.repositories.CategoryRepository;
 import com.devsaadeh.dscatalog.repositories.ProductRepository;
+import com.devsaadeh.dscatalog.services.exception.DatabaseException;
+import com.devsaadeh.dscatalog.services.exception.ResourceNotFoundException;
+import com.devsaadeh.dscatalog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.times;
 
 
 //When you've used a extension of your component like we have done with ProductService. We must mock components like
 // ProductRepository using @Mock instead of @MockBean.
+//@ExtendWith is used to make a unit test, in other words you are using only one component a part from others.
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
 
@@ -21,4 +38,56 @@ public class ProductServiceTests {
 
     @Mock
     private CategoryRepository categoryRepository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long dependentId;
+    private PageImpl<Product> page;
+    private Product product;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 2L;
+        dependentId = 3L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
+
+        Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).then(page);
+
+        //Mockito.doNothing().when(repository).deleteById(existingId);
+        Mockito.doThrow(ResourceNotFoundException.class).when(repository).deleteById(nonExistingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+
+        Mockito.when(repository.existsById(existingId)).thenReturn(true);
+        Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
+        Mockito.when(repository.existsById(dependentId)).thenReturn(true);
+
+    }
+
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId(){
+        Assertions.assertThrows(DatabaseException.class,()->{
+            service.delete(dependentId);
+        });
+    }
+
+    @Test
+    public void deleteShouldThrowExceptionWhenIdNotExists(){
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists(){
+        Assertions.assertDoesNotThrow(() ->{
+            service.delete(existingId);
+        });
+
+        Mockito.verify(repository, times(1)).deleteById(existingId);
+    }
 }
