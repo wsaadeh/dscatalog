ae59a88515e2402bdd34517ea3c99aaa9989fb5b
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-11-28 09:43:51 -0300

    adjusting dscatalogapplication.java

--- backend/src/main/java/com/devsaadeh/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsaadeh/dscatalog/DscatalogApplication.java
@@ -2,12 +2,11 @@ package com.devsaadeh.dscatalog;
 
 import com.devsaadeh.dscatalog.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  DscatalogApplication implements CommandLineRunner {
+public class  DscatalogApplication {
 
 	@Autowired
 	private S3Service service;
@@ -16,8 +15,5 @@ public class  DscatalogApplication implements CommandLineRunner {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		service.uploadFile("c:\\temp\\bruceLee.jpg");
-	}
+
 }
